86bb752e850c10b402ccc9ba0227e0c2cb1465a3
omlitt <michaelwl82@gmail.com>
2020-10-28 20:17:02 -0400

    studio assignment

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
-// TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=r27Vw5cmBe73mO0phoGNjWw2KNTl0yCg7Es4xK4Tt2E";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -24,8 +23,45 @@ const corsPromise = () =>
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      handleResponse(request.response);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+// const displayDiv = () => {
+// 	const wrapper = document.createElement("div");
+// 	wrapper.setAttribute("id", "wrapper");
+// 	const header = document.createElement("h1");
+// 	header.innerText = "My h1";
+// 	wrapper.appendChild(header);
+// 	outerDiv = document.getElementById("outer");
+// 	outerDiv.appendChild(wrapper);
+// }
+
+// const handleResponse = (requestResponse) => {
+// 	const jsonified = JSON.parse(requestResponse);
+// 	const plantsArray = jsonified.data;
+// 	const plantsAfter1753 = plantsArray.filter((arrayItem) => {
+//     	return arrayItem.year > 1753;
+//   	})
+
+//   	plantsAfter1753.map(console.log)
+// }
+
+const handleResponse = (requestResponse) => { 
+  const jsonified = JSON.parse(requestResponse);
+  const plants = jsonified.data;
+  filtered = plants.filter((arrayItem) =>
+    arrayItem.common_name < "E"
+    );
+  filtered.map((arrayItem) => {
+    const wrapper = document.createElement("div");
+    const image = document.createElement("img");
+    const title = document.createElement("h5");
+    title.innerText = arrayItem.common_name;
+    image.setAttribute("src", arrayItem.image_url);
+    wrapper.appendChild(title);
+    wrapper.appendChild(image);
+    document.getElementById("outer").appendChild(wrapper);
+  })
+}
